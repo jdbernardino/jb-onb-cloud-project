6dbba59efb30a8392816b46c607ad10b8120b82a
Jerome Bernardino <jerome.bernardino@orangeandbronze.com>
2018-11-04 19:30:00 +0800

    Changed user for psql.

--- src/main/java/com/orangeandbronze/repository/BalanceEntryDAO.java
+++ src/main/java/com/orangeandbronze/repository/BalanceEntryDAO.java
@@ -18,8 +18,8 @@ public class BalanceEntryDAO implements BalanceEntryRepository {
 	private static final String JDBC_URL = String
 			.format("jdbc:postgresql://google/%s?socketFactory=com.google.cloud.sql.postgres.SocketFactory"
 					+ "&cloudSqlInstance=%s", "eden-project-db", "edenproject-aacf3:asia-southeast1:eden-project");
-	private static final String USERNAME = "postgres";
-	private static final String PASSWORD = "postgres";
+	private static final String USERNAME = "eden-user";
+	private static final String PASSWORD = "eden-user";
 
 	private final Connection conn;
 
