2d11b2fbf54f7f2e953fe1366694508e38b5373a
Jerome Bernardino <jerome.bernardino@orangeandbronze.com>
2018-11-05 09:30:19 +0800

    Put all servlet mapping to web.xml.

--- src/main/java/com/orangeandbronze/controller/AddEntryController.java
+++ src/main/java/com/orangeandbronze/controller/AddEntryController.java
@@ -2,20 +2,15 @@ package com.orangeandbronze.controller;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.orangeandbronze.service.BalanceEntryDTO;
 import com.orangeandbronze.service.BalanceEntryService;
 import com.orangeandbronze.service.BalanceEntryServiceImpl;
 
-@WebServlet("/add")
 public class AddEntryController extends HttpServlet {
 
 	private BalanceEntryService service;
--- src/main/java/com/orangeandbronze/controller/HomeController.java
+++ src/main/java/com/orangeandbronze/controller/HomeController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +13,6 @@ import com.orangeandbronze.service.BalanceEntryDTO;
 import com.orangeandbronze.service.BalanceEntryService;
 import com.orangeandbronze.service.BalanceEntryServiceImpl;
 
-@WebServlet("/list")
 public class HomeController extends HttpServlet{
 	
 	private BalanceEntryService service;
